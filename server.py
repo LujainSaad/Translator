2af54c69331b51409028b8ff41c5fc04f6b01fe2
Lujain <79986157+LujainSaad@users.noreply.github.com>
2023-06-28 04:52:14 +0300

    fixing template

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from translator.translator import english_to_french, french_to_english
 
 app = Flask(__name__)
+app.template_folder = 'templates'
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
-@app.route('/translate/french-to-english/<text>')
-def translate_french_to_english(text):
-    translation = french_to_english(text)
-    return translation
+@app.route('/translate/french-to-english', methods=['POST'])
+def translate_french_to_english():
+    french_text = request.form.get('frenchText')
+    translation = french_to_english(french_text)
+    return render_template('index.html', translation=translation)
 
-@app.route('/translate/english-to-french/<text>')
-def translate_english_to_french(text):
-    translation = english_to_french(text)
-    return translation
+@app.route('/translate/english-to-french', methods=['POST'])
+def translate_english_to_french():
+    english_text = request.form.get('englishText')
+    translation = english_to_french(english_text)
+    return render_template('index.html', translation=translation)
 
 if __name__ == '__main__':
     app.run()
